21018a5c73b68aabb04586332c10f0d0e8076e13
HMilix <axel.wilfart@gmail.com>
2024-11-07 14:10:38 +0100

    Added packages

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,14 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>HMR</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button className="btn btn-primary" onClick={() => setCount((count) => count + 1)}>
+          count is N {count}
         </button>
+        <div className="bg-primary w-full">
+          a
+        </div>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
